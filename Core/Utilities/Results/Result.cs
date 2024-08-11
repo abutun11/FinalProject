532579db7266be035e24e047c8d380b6ebbe429b
abutun11 <butunalican01@gmail.com>
2024-08-12 01:04:09 +0300

    12.08.2024 - 9. Ders Kodları

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        //Normalde getter özellikli olan property set edilemez (readonlydir), sadece constructor içinde set edilebilir.
+
+        public Result(bool success, string message) : this(success)
+        {
+            Message = message;
+        }
+
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
