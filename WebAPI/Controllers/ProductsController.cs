ea4ecfd0f1f84c77cb7af57bfde006b55d6960b2
abutun11 <butunalican01@gmail.com>
2024-08-17 00:50:46 +0300

    17.08.2024 - 10. Ders Kodları

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,60 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //Attribute
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled (gevşek bağlılık)
+        //naming convention -- isimlendirme standartı
+        //fieldların defaultu private
+        //IoC Container -- Inversion of Control (Değişimin kontrolü)
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll() 
+        {
+            //Swagger
+            //Dependency chain
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("GetById")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("Add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
